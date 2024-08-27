9b1b63282c2208fdeb01f693169e0aee75febf5d
namestesensei <christopherwright435@gmail.com>
2024-08-27 08:44:27 +0000

    6.0

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("object-oriented"[0:] + " programming"[0:] + " with Python"[0:])
+str = "object-oriented programming with Python"
+str = str[0:6] + str[6:11] + str[11:13] + str[13:25] + str[25:32] + str[32:]
+print(str)
+
