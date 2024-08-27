433d73c790ae9e9fab7d809e2fc2e837be56e93d
namestesensei <christopherwright435@gmail.com>
2024-08-27 08:48:26 +0000

    6.1

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "object-oriented programming with Python"
-str = str[0:6] + str[6:11] + str[11:13] + str[13:25] + str[25:32] + str[32:]
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
-
