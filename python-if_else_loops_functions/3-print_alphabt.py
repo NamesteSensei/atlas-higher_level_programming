8fc0f0afa33c58376063a219165f1bb2bd3d4a63
namestesensei <christopherwright435@gmail.com>
2024-08-30 21:44:48 +0000

    3.0 print alpaha

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,7 @@
 
 # Loop through ASCII values for lowercase letters 'a' to 'z'
 for i in range(97, 123):
-    # Skip the letters 'e' (ASCII 101) and 'q' (ASCII 113)
-    if i != 101 and i != 113:
+    # Check if the current character is not 'e' (ASCII 101) and not 'q' (ASCII 113)
+    if chr(i) not in "eq":
         # Print the character corresponding to the current ASCII value without a newline
         print("{}".format(chr(i)), end="")
