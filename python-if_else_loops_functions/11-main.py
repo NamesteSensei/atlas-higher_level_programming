dab032547d894422431c31bdc85dc4f66d1d3c1a
namestesensei <christopherwright435@gmail.com>
2024-08-31 04:50:08 +0000

    11.0

---
+++ python-if_else_loops_functions/11-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Test script for the pow function
+
+pow = __import__('11-pow').pow
+
+# Test cases
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Function to compute a to the power of b and return the result
+
+def pow(a, b):
+    # Return the result of a raised to the power of b
+    return a ** b
