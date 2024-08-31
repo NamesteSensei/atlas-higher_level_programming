74e24ffca91748ab213f461fb8ad656da7d92405
namestesensei <christopherwright435@gmail.com>
2024-08-31 04:37:26 +0000

    9.0

---
+++ python-if_else_loops_functions/9-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Test script for the print_last_digit function
+
+print_last_digit = __import__('9-print_last_digit').print_last_digit
+
+# Test cases
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
