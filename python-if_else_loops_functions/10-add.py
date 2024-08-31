ac7c2550bdf3401949d492cfd3a4cc4e18f33611
namestesensei <christopherwright435@gmail.com>
2024-08-31 04:44:00 +0000

    10.0

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Function to add two integers and return the result
+
+def add(a, b):
+    # Return the sum of a and b
+    return a + b
---
+++ python-if_else_loops_functions/10-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Test script for the add function
+
+add = __import__('10-add').add
+
+# Test cases
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
