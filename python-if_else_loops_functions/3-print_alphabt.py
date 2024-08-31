7ab7737b38f402b8266cb40a7acc7cd9a5b9b9e3
namestesensei <christopherwright435@gmail.com>
2024-08-31 00:47:37 +0000

    3.6

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # This script prints the lowercase alphabet excluding 'q' and 'e'
 
-# Use a loop to iterate over ASCII values of lowercase letter# The chr function converts ASCII values back to characters
-# Use the en='' to avoid new line after printing 
+# Use a loop to iterate over ASCII values of lowercase letters
+# The chr function converts ASCII values back to characters
+# Use the end='' to avoid new line after printing
 
 for letter in range(97, 123):
     if letter != 101 and letter != 113:
