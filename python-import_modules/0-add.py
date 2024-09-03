607534011bdf66be7f63ee1fa5845937034cbb4d
namestesensei <christopherwright435@gmail.com>
2024-09-03 02:33:39 +0000

    0.9

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    # Importing the add function from add_0 module
-    from add_0 import add  # "add_0" appears only once here
+    from add_0 import add  # Import the add function from add_0.py
 
-    # Assign values to variables
-    a = 1  # a is assigned the value 1
-    b = 2  # b is assigned the value 2
-
-    # Print the result of the addition using string formatting
-    print("{} + {} = {}".format(a, b, add(a, b)))  # This prints: "1 + 2 = 3"
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
