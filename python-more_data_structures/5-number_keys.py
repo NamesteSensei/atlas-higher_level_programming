030f81945cf69ecbfa20e77e70304c827b1f87d8
namestesensei <christopherwright435@gmail.com>
2024-09-03 22:42:07 +0000

    5.0_number_keys

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+
+    :param a_dictionary: The dictionary to count keys in
+    :return: The number of keys in the dictionary
+    """
+    # Use the len() function to count the number of keys
+    return len(a_dictionary)
