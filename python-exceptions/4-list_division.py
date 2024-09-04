08d5e8c9742b8b03477ad931d62b3b4a06eef9dc
namestesensei <christopherwright435@gmail.com>
2024-09-04 00:09:18 +0000

    4-6

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    Divides elements of my_list_1 by my_list_2 up to list_length, handling various exceptions.
+
+    :param my_list_1: First list (numerators)
+    :param my_list_2: Second list (denominators)
+    :param list_length: Number of elements to divide
+    :return: A new list with division results, with 0 in case of errors
+    """
+    result_list = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except (TypeError, ValueError):
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally
---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError with the provided message.
+
+    :param message: The message to include with the NameError
+    """
+    raise NameError(message)
