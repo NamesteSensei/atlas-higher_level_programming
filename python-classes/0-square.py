4698578232fcbd961a0f6c686a1c75d9ffec5ad3
namestesensei <christopherwright435@gmail.com>
2024-09-09 00:56:27 +0000

    0 and 1 added shebang for python3

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Define an empty clas called Square
 class Square:
     pass # Placeholder, since the class is currently empty
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,9 @@
-# Defines a clas Square with a privare instance attribute 'size'
+#!/usr/bin/python3
+# 1-square.py
+# Defines a class Square with a private instance attribute 'size'
 
 class Square:
     def __init__(self, size):
         # Private instance attribute '__size'
-        self.__size = size
+        self.__size = size  # Name mangling to make 'size' private
+
