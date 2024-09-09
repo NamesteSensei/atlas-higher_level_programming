13c4fff487ecaf7dde407f7cacff7b67ef86e7a8
namestesensei <christopherwright435@gmail.com>
2024-09-09 07:41:12 +0000

    3..0

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,10 +11,10 @@ class Square:
     def __init__(self, size=0):
         """
         Initialize a Square instance with a private size attribute.
-        
+
         Args:
             size (int): The size of the square (default is 0).
-            
+
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
