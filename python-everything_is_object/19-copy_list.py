cc0b3cba1d2242a241fbd6c370785a73f2b55a0d
root <christopherwright435@gmail.com>
2024-09-15 23:30:34 +0000

    19.2

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,3 +1,13 @@
-def copy_list(a_list):
-    return a_list[:]
+#!/usr/bin/python3
+copy_list = __import__('19-copy_list').copy_list
 
+my_list = [1, 2, 3]
+print(my_list)
+
+new_list = copy_list(my_list)
+
+print(my_list)          # Prints the original list
+print(new_list)         # Prints the copied list
+
+print(new_list == my_list)  # True, because their contents are equal
+print(new_list is my_list)  # False, because they are different objects
