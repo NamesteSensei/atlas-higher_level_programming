2c20a45891524f5c00ea8859dd12ab7e09f38857
root <christopherwright435@gmail.com>
2024-09-15 23:43:03 +0000

    19.4

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-from 19-copy_list import copy_list; my_list = [1, 2, 3]; new_list = copy_list(my_list)
+def copy_list(lst): return lst[:]
+my_list = [1, 2, 3]; new_list = copy_list(my_list)
 print(my_list, new_list, new_list == my_list, new_list is my_list)
