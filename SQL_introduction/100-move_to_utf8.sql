fcee5363588da2d865f2d5099a1077f9594195b6
namestesensei <christopherwright435@gmail.com>
2024-10-14 13:52:12 -0500

    Task 17: Added script to convert database, table, and field to UTF8 (utf8mb4)

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,11 @@
+-- Task 17: Convert database, table, and field to utf8mb4
+-- This script converts the hbtn_0c_0 database, the first_table, and the name field to utf8mb4.
+
+-- Convert the database to utf8mb4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Convert the table to utf8mb4
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the 'name' field in first_table to utf8mb4
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
