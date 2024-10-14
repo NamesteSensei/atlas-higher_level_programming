ef7f46fdf6de6fac1dc3999f00df6408ea45d859
namestesensei <christopherwright435@gmail.com>
2024-10-14 13:16:25 -0500

    Task 4: Added script to create 'first_table' with id and name columns

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Task 4: Create first_table with id and name fields
+-- This script creates a table called first_table with columns id and name.
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
