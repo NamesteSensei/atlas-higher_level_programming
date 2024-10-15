8635ad9e653a008ba2527262cef984dc69e3533b
namestesensei <christopherwright435@gmail.com>
2024-10-15 18:20:43 -0500

    Completed Task 12: List shows without any genre.

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Task 12: Shows without a genre
+-- This script lists all shows that don't have any genre linked.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC;
