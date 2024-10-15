22fd846fb372c41c3ab23bd931afc0c7ec109052
namestesensei <christopherwright435@gmail.com>
2024-10-15 18:22:59 -0500

    Completed Task 14: List genres of the show 'Dexter'.

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Task 14: List genres of the show "Dexter"
+-- This script lists all genres of the show "Dexter."
+
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
