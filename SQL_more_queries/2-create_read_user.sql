1bbda4b9d8e3fc4082d425fb67d74aa93949200f
namestesensei <christopherwright435@gmail.com>
2024-10-14 19:32:17 -0500

    Task 2: Added script to create database hbtn_0d_2 and user_0d_2 with SELECT privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Task 2: Create database hbtn_0d_2 and user_0d_2 with SELECT privileges
+-- This script creates the database and user with limited privileges.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
