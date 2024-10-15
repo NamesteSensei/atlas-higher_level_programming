6a39b0446023bd706ea11eee42fb3edfa6c80c54
namestesensei <christopherwright435@gmail.com>
2024-10-14 19:27:26 -0500

    Task 0: Added script to list privileges for user_0d_1 and user_0d_2

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Task 0: List all privileges of user_0d_1 and user_0d_2
+-- This script lists the privileges for both users.
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
