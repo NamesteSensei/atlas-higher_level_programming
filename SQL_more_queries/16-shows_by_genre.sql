fa1ea0fdc8dcaf62848f3607a5720f224343502a
namestesensei <christopherwright435@gmail.com>
2024-10-15 18:25:18 -0500

    Completed Task 16: List shows and genres.

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Task 16: List shows and their genres
+-- This script lists all shows and their genres. If a show has no genre, display NULL.
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
