ec7485e274874dc61d78c7a6024bc85d91bf2598
namestesensei <christopherwright435@gmail.com>
2024-10-15 22:48:27 -0500

    Completed Task 0: Get all states from the database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
 
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
 
