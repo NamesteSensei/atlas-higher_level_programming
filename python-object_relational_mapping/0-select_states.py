1365d27f016001a9302f69bec9bb35323ff6272c
namestesensei <christopherwright435@gmail.com>
2024-10-16 23:33:48 -0500

    Fix script to ensure correct output for all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
-import os
 
 if __name__ == "__main__":
-    # Capture arguments
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=os.getenv('MYSQL_PWD'),  # Use environment variable for password
-        db=sys.argv[2]
-    )
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    # Create a cursor object to execute SQL queries
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC;")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+
+    # Execute the SQL query to retrieve all states
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the results of the query
+    states = cur.fetchall()
+
+    # Loop through the states and print them one by one
+    for state in states:
+        print(state)
+
+    # Close the cursor and database connection
     cur.close()
     db.close()
