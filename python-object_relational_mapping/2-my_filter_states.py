863219c2c890ba4783ba0734e09c17df586f206d
namestesensei <christopherwright435@gmail.com>
2024-10-16 23:44:20 -0500

    Add script to filter states by user input with matching name

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python3
-"""
-A script that displays all values in the states table where name matches the argument.
-"""
-
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials and state name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    # Arguments: mysql username, mysql password, database name, state name
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
-    # Connect to MySQL
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-
-    # Create a cursor to execute queries
     cur = db.cursor()
 
-    # Use format to create the SQL query with the user input
+    # Use format to create SQL query with user input safely
+    state_name = sys.argv[4]
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    cur.execute(query)
 
-    # Fetch and display the results
+    cur.execute(query)
     rows = cur.fetchall()
 
-    # Handle case: No records
-    if len(rows) == 0:
-        print("No records found.")
-    else:
-        for row in rows:
-            print(row)
+    for row in rows:
+        print(row)
 
-    # Close cursor and connection
     cur.close()
     db.close()
+
