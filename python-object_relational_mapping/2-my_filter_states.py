e1a03b3189ed93fa5ef97ce5b15c27349726951e
namestesensei <christopherwright435@gmail.com>
2024-10-16 23:54:53 -0500

    Fix PEP 8 issues and shorten comments

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,50 @@
 #!/usr/bin/python3
+"""
+Script that lists all states where 'name' matches the argument from the
+database 'hbtn_0e_0_usa'. Uses MySQLdb to connect to MySQL server.
+Arguments:
+    mysql username (sys.argv[1])
+    mysql password (sys.argv[2])
+    database name (sys.argv[3])
+    state name (sys.argv[4])
+"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Arguments: mysql username, mysql password, database name, state name
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    # Command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    # Create a cursor to execute queries
     cur = db.cursor()
 
-    # Use format to create SQL query with user input safely
-    state_name = sys.argv[4]
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    # SQL query to find states by name (case-sensitive)
+    query = (
+        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
+        .format(state_name)
+    )
 
+    # Execute the query and fetch results
     cur.execute(query)
     rows = cur.fetchall()
 
+    # Print each row
     for row in rows:
         print(row)
 
+    # Close the cursor and connection
     cur.close()
     db.close()
-
