4acd8d568f2d1901e81f0dc536802ddda39c20d0
namestesensei <christopherwright435@gmail.com>
2024-11-19 20:09:26 -0600

    Completed Task 5: 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-const arg = parseInt(process.argv[2]);
-console.log(isNaN(arg) ? "Not a number" : `My number: ${arg}`);
+const arg = process.argv[2];
+const num = parseInt(arg);
+
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}
