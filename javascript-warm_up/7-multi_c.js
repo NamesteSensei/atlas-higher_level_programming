42e0433205ca1c108a52ca9a45f5128465f64ed5
namestesensei <christopherwright435@gmail.com>
2024-11-19 20:34:48 -0600

    Completed Task 7: 7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,9 @@ const x = parseInt(process.argv[2]);
 if (isNaN(x) || x <= 0) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  let i = 0;
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }
