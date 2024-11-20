5903ea3c1b51b4c5f0b099a50410b7957905e7df
namestesensei <christopherwright435@gmail.com>
2024-11-19 20:57:49 -0600

    Completed Task 10: 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-function factorial(n) {
-  if (isNaN(n) || n <= 1) return 1;
-  return n * factorial(n - 1);
+const arg = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-console.log(factorial(parseInt(process.argv[2])));
+
+console.log(factorial(arg));
