ca3739123999b02e7d6b906dddc403e94ebc96fd
namestesensei <christopherwright435@gmail.com>
2024-11-19 20:52:10 -0600

    Completed Task 9: 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
-function add(a, b) {
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+function add (a, b) {
   return a + b;
 }
-console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
+
+if (isNaN(arg1) || isNaN(arg2)) {
+  console.log('NaN');
+} else {
+  console.log(add(arg1, arg2));
+}
